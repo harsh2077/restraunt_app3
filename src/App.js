3e425d48224d7162b6a13a6e5c2d4ce26e8ed47a
harsh2077 <110387163+harsh2077@users.noreply.github.com>
2023-10-20 00:00:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,8 +21,4 @@ function App() {
     </div>
   );
 }
-
-
-
-
 export default App;
